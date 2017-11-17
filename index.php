6fbc688f48bcf90cf8eff727925fc02465f1fd22
ronald_araujo <f.ronaldaraujo@gmail.com>
2017-11-17 18:47:23 -0300

    First commit

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+$jsondata = file_get_contents('one_tbl.json');
+$tbls = json_decode($jsondata, true);
+
+public function ProcessTable()
+{
+	# code...
+}
+
+// var_dump($tbls);
+
+foreach ($tbls as $key => $table) {
+	var_dump($key);
+
+}
